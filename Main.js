144edda2a5bc36c58a425391d1ba8d6695b65a56
RemiBougie <148083592+RemiBougie@users.noreply.github.com>
2023-10-27 09:22:00 -0500

    Create Main.js

---
+++ Main.js
@@ -0,0 +1,125 @@
+const http = require('http');
+const https = require('https');
+const { createRequest } = require('./sample.js');
+//const { apiRequest } = require('./apiRequest.js');
+/*
+const options = {
+  hostname: 'wkx8abizkk.execute-api.us-east-2.amazonaws.com',
+  path: '/dev/s3GetObj-Test',
+  method: 'GET',
+  protocol: 'https:',
+  headers: {
+    'Content-Type': 'application/json',
+    host: 'wkx8abizkk.execute-api.us-east-2.amazonaws.com'
+  }
+};
+*/
+//const req = createRequest(options);
+
+http.createServer( (req, res) => {
+  const options = {
+    hostname: 'wkx8abizkk.execute-api.us-east-2.amazonaws.com',
+    path: '/dev/s3GetObj-Test',
+    method: 'GET',
+    protocol: 'https:',
+    headers: {
+      'Content-Type': 'application/json',
+      host: 'wkx8abizkk.execute-api.us-east-2.amazonaws.com'
+    }
+  };
+
+  const signedReq = createRequest(options);
+
+  const apiResponse = new Promise((resolve, reject) => {
+    const apiRequest = http.request(signedReq, (response) => {
+      response.setEncoding("utf8");
+      let responseBody = "";
+
+      response.on("data", (chunk) => {
+        responseBody += chunk;
+      });
+
+      response.on("end", () => {
+        resolve(JSON.parse(responseBody));
+      });
+    });
+
+    apiRequest.on("error", (err) => {
+      reject(err);
+    });
+
+    apiRequest.end();
+  }).then((value) => {
+
+  let body = [];
+
+  req.on('error', err => {
+    console.error(err);
+  });
+
+  req.on('data', (chunk) => {
+    body.push(chunk);
+  });
+
+  req.on('end', () => {
+    body = Buffer.concat(body).toString();
+    res.on('error', err => {
+      console.error(err);
+    });
+
+    res.writeHead(200, {'Content-Type': 'application/json'});
+    res.write('Hello World! Quick change');
+    res.write(JSON.stringify(signedReq));
+    res.write(JSON.stringify(value));
+    res.end();
+})
+  });
+/*
+  let body = [];
+    request
+      .on('error', err => {
+        console.error(err);
+      })
+      .on('data', chunk => {
+        body.push(chunk);
+      })
+      .on('end', () => {
+        body = Buffer.concat(body).toString();
+        // BEGINNING OF NEW STUFF
+ 
+        response.on('error', err => {
+          console.error(err);
+        });
+
+  let Bucket = 's3-swim-app-super-beta';
+  let Key = 'set_1.html';
+
+  let apiRequest = createRequest(options);
+  console.log(apiRequest);
+/*
+  const apiResponse = new Promise((resolve, reject) => {
+    const request = https.request(apiRequest, (response) => {
+      response.setEncoding("utf8");
+      let responseBody = "";
+
+      response.on("data", (chunk) => {
+        responseBody += chunk;
+      });
+
+      response.on("end", () => {
+        resolve(JSON.parse(responseBody));
+      });
+    });
+
+    request.on("error", (err) => {
+      reject(err);
+    });
+
+    request.end();
+  });
+
+  console.log(apiResponse);
+
+  res.write(apiResponse)
+*/
+}).listen(8080);
