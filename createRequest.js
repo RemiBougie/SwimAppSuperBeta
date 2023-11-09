4fd80b3cdcefae4ee75e203bd4f3c6b8d740eb73
RemiBougie <148083592+RemiBougie@users.noreply.github.com>
2023-11-09 11:39:52 -0600

    Update createRequest.js

--- createRequest.js
+++ createRequest.js
@@ -5,30 +5,36 @@ const crt = require('@aws-sdk/signature-v4-crt');
 const { Hash } = require('@aws-sdk/hash-node');
 
 async function createRequest (options) {
-  // GET AWS CREDENTIALS FROM EC2 INSTANCE METADATA
-  const provider = fromInstanceMetadata();
-  const cred = await provider();
-  console.log(cred);
-
-  // SHA256 HASH CONSTRUCTOR USING SECRET ACCESS KEY
-  const SHA256 = new Hash('sha256', cred.secretAccessKeyId)
-
-  // CREATING SIGNER
-  const crtSignerInit = {
-    credentials: cred,
-    region: 'us-east-2',
-    service: 'execute-api',
-    applyChecksum: false,
-    sha256: SHA256,
-    signingAlgorithm: 'SigV4',
-    uriEscapePath: true
+  return new Promise((resolve, reject) => {
+    try {
+      // GET AWS CREDENTIALS FROM EC2 INSTANCE METADATA
+      const provider = fromInstanceMetadata();
+      const cred = await provider();
+      console.log(cred);
+    
+      // SHA256 HASH CONSTRUCTOR USING SECRET ACCESS KEY
+      const SHA256 = new Hash('sha256', cred.secretAccessKeyId)
+    
+      // CREATING SIGNER
+      const crtSignerInit = {
+        credentials: cred,
+        region: 'us-east-2',
+        service: 'execute-api',
+        applyChecksum: false,
+        sha256: SHA256,
+        signingAlgorithm: 'SigV4',
+        uriEscapePath: true
+      }
+      const crtSigner = new crt.CrtSignerV4(crtSignerInit)
+    
+      // SIGN HTTP REQUEST
+      const signedReq = await crtSigner.sign(options)
+      
+      resolve(signedReq);
+    } catch (err) {
+      reject(err);
+    };
   }
-  const crtSigner = new crt.CrtSignerV4(crtSignerInit)
-
-  // SIGN HTTP REQUEST
-  const signedReq = await crtSigner.sign(options)
-
-  return signedReq;
 }
 
 module.exports = { createRequest };
