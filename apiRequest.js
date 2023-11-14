17b7f48898cfff6d609ab3f664660473d1943c09
RemiBougie <148083592+RemiBougie@users.noreply.github.com>
2023-11-14 09:36:49 -0600

    Create apiRequest.js

---
+++ apiRequest.js
@@ -0,0 +1,42 @@
+const http = require('http');
+const https = require('https');
+const { createRequest } = require('./createRequest.js');
+
+// CREATE SIGNED REQUEST TO API GATEWAY
+const options = {
+  hostname: 'wkx8abizkk.execute-api.us-east-2.amazonaws.com',
+  path: '/dev/s3GetObj-Test',
+  method: 'GET',
+  protocol: 'https:',
+  headers: {
+    'Content-Type': 'application/json',
+    host: 'wkx8abizkk.execute-api.us-east-2.amazonaws.com'
+  }
+};
+const signedReq = createRequest(options);
+
+async function apiRequest (signedRequest) {
+  return new Promise((resolve, reject) => {
+    const apiRequest = http.request(signedRequest, (response) => {
+      response.setEncoding("utf8");
+      let responseBody = "";
+  
+      response.on("data", (chunk) => {
+        responseBody += chunk;
+      });
+  
+      response.on("end", () => {
+        console.log(responseBody);
+        resolve(JSON.stringify(responseBody));
+      });
+    });
+  
+    apiRequest.on("error", (err) => {
+      reject(err);
+    });
+  
+    apiRequest.end();
+  });
+}
+
+apiRequest(signedReq);
